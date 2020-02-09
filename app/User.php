f7f9a1076a1021045a1d46817f5631cddef41f8d
josegomz <josegomz.sa17@gmail.com>
2020-02-09 08:12:58 -0600

    autorización de roles

--- app/User.php
+++ app/User.php
@@ -15,7 +15,40 @@ class User extends Authenticatable
         return $this->belongsToMany('App\Role');
     }
 
+    /*validacion de roles*/
+    public function authorizeRoles($roles){
+        if ($this->hasAnyRole($roles)) {
+            return true;
+        }
+        abort(401, 'Esta acción no es autorizada'); 
+    }
+
+    /*verifica si tiene un arreglo de roles*/
+    public function hasAnyRole($roles){
+        if (is_array($roles)) {
+            foreach ($roles as $role) {
+                if ($this->hasRole($roles)) {
+                    return true;
+                }
+            }
+        }else{
+            if ($this->hasRole($roles)) {
+                return true;
+            }
+        }
+        return false; 
+    }
+
+
     /*validar si un usuario contiene el rol*/
+    public function hasRole($role){
+        if ($this->roles()->where('name',$role)->first()) {
+            # code...
+            return true;
+        }
+        return false;
+
+    }
 
     /**
      * The attributes that are mass assignable.
